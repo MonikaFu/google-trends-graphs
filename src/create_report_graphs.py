a9d4005488f108b7f9b2b43c96bd7a30eaa7c152
Monika Furdyna <monika.furdyna@probability.nl>
2020-07-02 11:13:14 +0200

    fixed paths in main script

--- src/create_report_graphs.py
+++ src/create_report_graphs.py
@@ -9,7 +9,7 @@ from settings import path
 
 import os
 
-os.chdir(path+'/data')
+os.chdir(path+'/dataset')
 from make_dataset import make_dataset_for_plots
 
 os.chdir(path+'/visualization')
@@ -21,11 +21,11 @@ os.chdir(path)
 
 name_common = 'multiTimeline_'
 
-name_categories = ['Climate','Climate_topics','Education_Online','Energy_topics',
-                   'Exercise_Youtube','Food','Food_Delivery','IT_security_topics',
-                   'Public_Transport','Remote_Work','Stay_Home','Sustainability_topics',
-                   'Vegan_topics']
-
+#name_categories = ['Climate','Climate_topics','Education_Online','Energy_topics',
+#                   'Exercise_Youtube','Food','Food_Delivery','IT_security_topics',
+#                   'Public_Transport','Remote_Work','Stay_Home','Sustainability_topics',
+#                   'Vegan_topics']
+name_categories = ['Climate_topics']
 for cat in name_categories:
      # import and process data
     data = make_dataset_for_plots('../data/raw_report/'+name_common+ cat +'.csv',': (Worldwide)')
@@ -35,7 +35,7 @@ for cat in name_categories:
     else:
         fig = draw_pretty_covid_graph_world(data)
     #plt.savefig('../../report/figures/'+'searches_over_time_'+cat+'.svg', bbox_inches='tight') # higher quality
-    plt.savefig('../report/figures_report/'+'searches_over_time_'+cat+'.png', bbox_inches='tight')
+#    plt.savefig('../report/figures_report/'+'searches_over_time_'+cat+'.png', bbox_inches='tight')
     
     plt.show()
         
